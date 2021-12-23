9d7b503d0422f3cce75a9893d7700f6dfe972b12
PasinduManeka <manekaherat815@gmail.com>
2021-12-23 16:15:14 +0530

    First commit create the database connection

---
+++ WebApplication1/Migrations/20211223103727_AddBookMVC.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class AddBookMVC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Author = table.Column<string>(nullable: false),
+                    ISBN = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
---
+++ WebApplication1/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+
+        //can't be null
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Author { get; set; }
+        [Required]
+        public String ISBN { get; set; }
+
+
+    }
+}
