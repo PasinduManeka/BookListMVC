1ea964239ecccd129a5a3b2870896231f5482447
PasinduManeka <manekaherat815@gmail.com>
2021-12-23 17:29:30 +0530

    ADDED APISin controller

--- WebApplication1/Controllers/BooksController.cs
+++ WebApplication1/Controllers/BooksController.cs
@@ -1,16 +1,55 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class BooksController : Controller
     {
+        //create a object of the applicationDBContext for databse connection
+        private readonly ApplicationDBContext _db;
+
+        //initialize the constructor. Use the dependency injection
+        public BooksController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API Calls        
+        //Get data
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            //get the book from DB related to the ID
+            var bookFromDB = await _db.Book.FirstOrDefaultAsync(u => u.ID == id);
+
+            //null
+            if (bookFromDB == null)
+            {
+                return Json(new { success = false, message = "Error while Deleting" });
+            }
+
+            //not null
+            _db.Book.Remove(bookFromDB);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Deleted Successfully!!" });
+
+        }
+        #endregion
     }
 }
